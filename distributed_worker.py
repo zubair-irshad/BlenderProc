460ae43417591ff6b2ada84693f31a5d4491fcbe
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-05-13 22:50:55 -0400

    modified data collect

--- distributed_worker.py
+++ distributed_worker.py
@@ -6,9 +6,9 @@ import subprocess
 def main(args):
     frame_nums = [int(x) for x in args.frames.split(",")]
     for scene_idx in frame_nums:
-        print("scene_idx", scene_idx)
-        # cmd = f"CUDA_VISIBLE_DEVICES={args.gpu} python cli.py run ./scripts/render_scene.py -s {scene_idx} --gpu {args.gpu}"
-        # subprocess.run(cmd, shell=True)
+        # print("scene_idx", scene_idx)
+        cmd = f"CUDA_VISIBLE_DEVICES={args.gpu} python cli.py run ./scripts/render_scene.py -s {scene_idx} --gpu {args.gpu}"
+        subprocess.run(cmd, shell=True)
 
 
 if __name__ == "__main__":
