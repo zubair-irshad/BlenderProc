ad95a14eea6c1195fa9e77aea39f1c451470039a
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-05-13 22:39:56 -0400

    modified data collect

--- distributed_worker.py
+++ distributed_worker.py
@@ -5,14 +5,15 @@ import subprocess
 
 def main(args):
     for scene_idx in args.frames:
-        cmd = f"CUDA_VISIBLE_DEVICES={args.gpu} python cli.py run ./scripts/render_scene.py -s {scene_idx} --gpu {args.gpu}"
-        subprocess.run(cmd, shell=True)
+        print("scene_idx", scene_idx)
+        # cmd = f"CUDA_VISIBLE_DEVICES={args.gpu} python cli.py run ./scripts/render_scene.py -s {scene_idx} --gpu {args.gpu}"
+        # subprocess.run(cmd, shell=True)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--frames", default=0, type=list, help="Is tested on validation data or not."
+        "--frames", type=list, help="Is tested on validation data or not."
     )
     # parser.add_argument(
     #     "--end", default=0, type=int, help="Is tested on validation data or not."
