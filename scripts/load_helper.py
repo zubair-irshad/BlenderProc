ee4c959bd234998d7164dd0f7e8661904068f6e0
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-05-16 01:01:29 -0400

    run on p2 instance 2200-2300

--- scripts/load_helper.py
+++ scripts/load_helper.py
@@ -11,9 +11,9 @@ import json
 # TEXTURE_DIR = '/data/yliugu/3D-FRONT-texture'
 # MODEL_DIR = '/data/yliugu/3D-FUTURE-model'
 
-LAYOUT_DIR = "/wild6d_data/zubair/3DFRONT_Raw/3D-FRONT"
-TEXTURE_DIR = "/wild6d_data/zubair/3DFRONT_Raw/3D-FRONT-texture"
-MODEL_DIR = "/wild6d_data/zubair/3DFRONT_Raw/3D-FUTURE-model"
+# LAYOUT_DIR = "/wild6d_data/zubair/3DFRONT_Raw/3D-FRONT"
+# TEXTURE_DIR = "/wild6d_data/zubair/3DFRONT_Raw/3D-FRONT-texture"
+# MODEL_DIR = "/wild6d_data/zubair/3DFRONT_Raw/3D-FUTURE-model"
 
 # LAYOUT_DIR = "/home/mirshad7/Downloads/3D-FRONT"
 # TEXTURE_DIR = "/home/mirshad7/Downloads/3D-FRONT-texture"
