2a23bcdda6aa2f0cb58f1cd085a4c12f732c6769
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-05-16 12:30:01 -0400

    generate low res

--- scripts/render_configs.py
+++ scripts/render_configs.py
@@ -1,10 +1,10 @@
 import numpy as np
 import yaml
 
-IMG_WIDTH = 640
-IMG_HEIGHT = 480
+# IMG_WIDTH = 640
+# IMG_HEIGHT = 480
 
-K = np.array([[400, 0, 320], [0, 400, 240], [0, 0, 1]])
+# K = np.array([[400, 0, 320], [0, 400, 240], [0, 0, 1]])
 
 # OBJ_BAN_LIST is a global keyword ban list for all the rooms
 OBJ_BAN_LIST = [
