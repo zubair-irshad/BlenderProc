33c5351da968e36388b9b6309595bc1d623c6ad3
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-05-18 16:24:58 -0400

    OPTIX

--- distributed_2.py
+++ distributed_2.py
@@ -60,8 +60,8 @@ if __name__ == "__main__":
     queue = multiprocessing.JoinableQueue()
     count = multiprocessing.Value("i", 0)
 
-    start_scene_idx = 2300
-    end_scene_idx = 2400
+    start_scene_idx = 2400
+    end_scene_idx = 2500
     worker_per_gpu = 5
     # num_gpus = 8  # 6
     # gpu_start = 0  # 2
