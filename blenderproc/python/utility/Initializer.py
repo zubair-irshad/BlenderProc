af4600e62fb1edfc5182f46a966de67303a2dd60
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-05-18 01:07:12 -0400

    OPTIX

--- blenderproc/python/utility/Initializer.py
+++ blenderproc/python/utility/Initializer.py
@@ -14,8 +14,14 @@ import addon_utils
 import blenderproc.python.renderer.RendererUtility as RendererUtility
 
 
-def init(horizon_color: list = [0.05, 0.05, 0.05], compute_device: str = "GPU", compute_device_type: str = None, use_experimental_features: bool = False, clean_up_scene: bool = True):
-    """ Initializes basic blender settings, the world and the camera.
+def init(
+    horizon_color: list = [0.05, 0.05, 0.05],
+    compute_device: str = "GPU",
+    compute_device_type: str = None,
+    use_experimental_features: bool = False,
+    clean_up_scene: bool = True,
+):
+    """Initializes basic blender settings, the world and the camera.
 
     Also cleans up the whole scene at first.
 
@@ -25,6 +31,9 @@ def init(horizon_color: list = [0.05, 0.05, 0.05], compute_device: str = "GPU", 
     :param use_experimental_features: Set to True, if you want to use the Experimental features of the Cycles Render Engine i.e Adaptive subdivision. (default: ``False``).
     :param clean_up_scene: Set to False, if you want to keep all scene data.
     """
+    print("==========================================================\n\n\n")
+    print("HEREEEEEEEEEEEEEEEEEEEEEEE")
+    print("==========================================================\n\n\n")
     if clean_up_scene:
         cleanup()
 
@@ -33,24 +42,35 @@ def init(horizon_color: list = [0.05, 0.05, 0.05], compute_device: str = "GPU", 
         print("Setting blender language settings to english during this run")
         bpy.context.preferences.view.language = "en_US"
 
-    prefs = bpy.context.preferences.addons['cycles'].preferences
+    prefs = bpy.context.preferences.addons["cycles"].preferences
     # Use cycles
-    bpy.context.scene.render.engine = 'CYCLES'
+    bpy.context.scene.render.engine = "CYCLES"
 
     if platform == "darwin":
         import platform as platform_locally
+
         mac_version = platform_locally.mac_ver()[0]
         mac_version_numbers = [int(ele) for ele in mac_version.split(".")]
-        if (mac_version_numbers[0] == 12 and mac_version_numbers[1] >= 3) or mac_version_numbers[0] > 12:
+        if (
+            mac_version_numbers[0] == 12 and mac_version_numbers[1] >= 3
+        ) or mac_version_numbers[0] > 12:
             bpy.context.scene.cycles.device = "GPU"
-            preferences = bpy.context.preferences.addons['cycles'].preferences
+            preferences = bpy.context.preferences.addons["cycles"].preferences
             for device_type in preferences.get_device_types(bpy.context):
                 preferences.get_devices_for_type(device_type[0])
             gpu_type = "METAL"  # only available type on mac os
             for device in preferences.devices:
-                if device.type == gpu_type and (compute_device_type is None or compute_device_type == gpu_type):
-                    bpy.context.preferences.addons['cycles'].preferences.compute_device_type = gpu_type
-                    print('Device {} of type {} found and used.'.format(device.name, device.type))
+                if device.type == gpu_type and (
+                    compute_device_type is None or compute_device_type == gpu_type
+                ):
+                    bpy.context.preferences.addons[
+                        "cycles"
+                    ].preferences.compute_device_type = gpu_type
+                    print(
+                        "Device {} of type {} found and used.".format(
+                            device.name, device.type
+                        )
+                    )
                     break
             # make sure that all visible GPUs are used
             for device in prefs.devices:
@@ -65,15 +85,23 @@ def init(horizon_color: list = [0.05, 0.05, 0.05], compute_device: str = "GPU", 
         bpy.context.scene.render.threads = multiprocessing.cpu_count()
     else:
         bpy.context.scene.cycles.device = "GPU"
-        preferences = bpy.context.preferences.addons['cycles'].preferences
+        preferences = bpy.context.preferences.addons["cycles"].preferences
         for device_type in preferences.get_device_types(bpy.context):
             preferences.get_devices_for_type(device_type[0])
         for gpu_type in ["OPTIX", "CUDA"]:
             found = False
             for device in preferences.devices:
-                if device.type == gpu_type and (compute_device_type is None or compute_device_type == gpu_type):
-                    bpy.context.preferences.addons['cycles'].preferences.compute_device_type = gpu_type
-                    print('Device {} of type {} found and used.'.format(device.name, device.type))
+                if device.type == gpu_type and (
+                    compute_device_type is None or compute_device_type == gpu_type
+                ):
+                    bpy.context.preferences.addons[
+                        "cycles"
+                    ].preferences.compute_device_type = gpu_type
+                    print(
+                        "Device {} of type {} found and used.".format(
+                            device.name, device.type
+                        )
+                    )
                     found = True
                     break
             if found:
@@ -84,14 +112,14 @@ def init(horizon_color: list = [0.05, 0.05, 0.05], compute_device: str = "GPU", 
 
     # Set the Experimental features on/off
     if use_experimental_features:
-        bpy.context.scene.cycles.feature_set = 'EXPERIMENTAL'
+        bpy.context.scene.cycles.feature_set = "EXPERIMENTAL"
 
     # setting the frame end, will be changed by the camera loader modules
     bpy.context.scene.frame_end = 0
 
     # Sets background color
     RendererUtility.set_world_background(horizon_color)
-    world = bpy.data.worlds['World']
+    world = bpy.data.worlds["World"]
     world["category_id"] = 0
 
     # Create the camera
@@ -112,11 +140,12 @@ def init(horizon_color: list = [0.05, 0.05, 0.05], compute_device: str = "GPU", 
         random.seed(random_seed)
         np_random.seed(random_seed)
 
+
 def cleanup():
-    """ Resets the scene to its clean state, but keeping the UI as it is """
+    """Resets the scene to its clean state, but keeping the UI as it is"""
     # Switch to right context
     if bpy.context.object is not None and bpy.context.object.mode != "OBJECT":
-        bpy.ops.object.mode_set(mode='OBJECT')
+        bpy.ops.object.mode_set(mode="OBJECT")
 
     # Clean up
     Initializer._remove_all_data()
@@ -126,16 +155,29 @@ def cleanup():
     new_world = bpy.data.worlds.new("World")
     bpy.context.scene.world = new_world
 
-class Initializer:
 
+class Initializer:
     @staticmethod
     def set_default_parameters():
-        """ Loads and sets default parameters defined in DefaultConfig.py """
+        """Loads and sets default parameters defined in DefaultConfig.py"""
         # Set default intrinsics
-        CameraUtility.set_intrinsics_from_blender_params(DefaultConfig.fov, DefaultConfig.resolution_x, DefaultConfig.resolution_y, 
-                                                         DefaultConfig.clip_start, DefaultConfig.clip_end, DefaultConfig.pixel_aspect_x, 
-                                                         DefaultConfig.pixel_aspect_y, DefaultConfig.shift_x, DefaultConfig.shift_y, DefaultConfig.lens_unit)
-        CameraUtility.set_stereo_parameters(DefaultConfig.stereo_convergence_mode, DefaultConfig.stereo_convergence_distance, DefaultConfig.stereo_interocular_distance)
+        CameraUtility.set_intrinsics_from_blender_params(
+            DefaultConfig.fov,
+            DefaultConfig.resolution_x,
+            DefaultConfig.resolution_y,
+            DefaultConfig.clip_start,
+            DefaultConfig.clip_end,
+            DefaultConfig.pixel_aspect_x,
+            DefaultConfig.pixel_aspect_y,
+            DefaultConfig.shift_x,
+            DefaultConfig.shift_y,
+            DefaultConfig.lens_unit,
+        )
+        CameraUtility.set_stereo_parameters(
+            DefaultConfig.stereo_convergence_mode,
+            DefaultConfig.stereo_convergence_distance,
+            DefaultConfig.stereo_interocular_distance,
+        )
 
         # Init renderer
         RendererUtility._render_init()
@@ -147,30 +189,39 @@ class Initializer:
         RendererUtility.set_cpu_threads(0)
         RendererUtility.set_denoiser(DefaultConfig.denoiser)
 
-        RendererUtility.set_simplify_subdivision_render(DefaultConfig.simplify_subdivision_render)
+        RendererUtility.set_simplify_subdivision_render(
+            DefaultConfig.simplify_subdivision_render
+        )
 
-        RendererUtility.set_light_bounces(DefaultConfig.diffuse_bounces,
-                                          DefaultConfig.glossy_bounces,
-                                          DefaultConfig.ao_bounces_render,
-                                          DefaultConfig.max_bounces,
-                                          DefaultConfig.transmission_bounces,
-                                          DefaultConfig.transparency_bounces,
-                                          DefaultConfig.volume_bounces)
+        RendererUtility.set_light_bounces(
+            DefaultConfig.diffuse_bounces,
+            DefaultConfig.glossy_bounces,
+            DefaultConfig.ao_bounces_render,
+            DefaultConfig.max_bounces,
+            DefaultConfig.transmission_bounces,
+            DefaultConfig.transparency_bounces,
+            DefaultConfig.volume_bounces,
+        )
 
-        RendererUtility.set_output_format(DefaultConfig.file_format,
-                                          DefaultConfig.color_depth,
-                                          DefaultConfig.enable_transparency,
-                                          DefaultConfig.jpg_quality)
-
+        RendererUtility.set_output_format(
+            DefaultConfig.file_format,
+            DefaultConfig.color_depth,
+            DefaultConfig.enable_transparency,
+            DefaultConfig.jpg_quality,
+        )
 
     @staticmethod
     def _remove_all_data():
-        """ Remove all data blocks except opened scripts and the default scene. """
+        """Remove all data blocks except opened scripts and the default scene."""
         # Go through all attributes of bpy.data
         for collection in dir(bpy.data):
             data_structure = getattr(bpy.data, collection)
             # Check that it is a data collection
-            if isinstance(data_structure, bpy.types.bpy_prop_collection) and hasattr(data_structure, "remove") and collection not in ["texts"]:
+            if (
+                isinstance(data_structure, bpy.types.bpy_prop_collection)
+                and hasattr(data_structure, "remove")
+                and collection not in ["texts"]
+            ):
                 # Go over all entities in that collection
                 for block in data_structure:
                     # Remove everything besides the default scene
@@ -179,6 +230,6 @@ class Initializer:
 
     @staticmethod
     def _remove_custom_properties():
-        """ Remove all custom properties registered at global entities like the scene. """
+        """Remove all custom properties registered at global entities like the scene."""
         for key in bpy.context.scene.keys():
             del bpy.context.scene[key]
