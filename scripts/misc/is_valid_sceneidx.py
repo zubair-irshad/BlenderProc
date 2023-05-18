4b87ab92e6b2d79ee12d484eb3a8d1a0498451b8
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-05-18 17:41:23 -0400

    OPTIX

---
+++ scripts/misc/is_valid_sceneidx.py
@@ -0,0 +1,19 @@
+import os
+import yaml
+
+start_idx = 2300
+end_idx = 2400
+folder_path = "/home/ubuntu/BlenderProc/scripts/all_is_valid"
+
+valid_files = []
+
+for idx in range(start_idx, end_idx + 1):
+    file_path = os.path.join(folder_path, f"is_valid_{idx}.yaml")
+    if os.path.isfile(file_path):
+        with open(file_path, "r") as file:
+            data = yaml.safe_load(file)
+            if data.get("value") == "true":
+                valid_files.append(idx)
+
+print(valid_files)
+print(len(valid_files))
