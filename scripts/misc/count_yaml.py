e6bc15b418758310fed7736a85e002d9dc0660fb
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-05-19 13:35:44 -0400

    generate valid poses again

--- scripts/misc/count_yaml.py
+++ scripts/misc/count_yaml.py
@@ -9,30 +9,36 @@ directory_path_valid = "/home/ubuntu/zubair/BlenderProc/scripts/all_valid_boxes"
 
 start = 2100
 end = 2200
-# Loop through each YAML file from 2100 to 2200
-for i in range(start, end):
-    filename = os.path.join(directory_path, f"bbox_{i}.yaml")
-    try:
-        with open(filename, "r") as file:
-            data = yaml.safe_load(file)
-            # Count the number of subentries in the YAML file
-            subentry_count += len(data[i].keys())
-    except FileNotFoundError:
-        continue
 
-print("Total subentry count:", subentry_count)
+idxs = [2300, 2400, 2500]
 
+for i in range(len(idxs) - 1):
+    start = idxs[i]
+    end = idxs[i + 1]
+    print("=================idxs", start, end, "=================\n\n\n")
+    # Loop through each YAML file from 2100 to 2200
+    for i in range(start, end):
+        filename = os.path.join(directory_path, f"bbox_{i}.yaml")
+        try:
+            with open(filename, "r") as file:
+                data = yaml.safe_load(file)
+                # Count the number of subentries in the YAML file
+                subentry_count += len(data[i].keys())
+        except FileNotFoundError:
+            continue
 
-subentry_count = 0
-# Loop through each YAML file from 2100 to 2200
-for i in range(start, end):
-    filename = os.path.join(directory_path_valid, f"bbox_{i}.yaml")
-    try:
-        with open(filename, "r") as file:
-            data = yaml.safe_load(file)
-            # Count the number of subentries in the YAML file
-            subentry_count += len(data[i].keys())
-    except FileNotFoundError:
-        continue
+    print("Total subentry count:", subentry_count)
 
-print("Total subentry count valid:", subentry_count)
+    subentry_count = 0
+    # Loop through each YAML file from 2100 to 2200
+    for i in range(start, end):
+        filename = os.path.join(directory_path_valid, f"bbox_{i}.yaml")
+        try:
+            with open(filename, "r") as file:
+                data = yaml.safe_load(file)
+                # Count the number of subentries in the YAML file
+                subentry_count += len(data[i].keys())
+        except FileNotFoundError:
+            continue
+
+    print("Total subentry count valid:", subentry_count)
