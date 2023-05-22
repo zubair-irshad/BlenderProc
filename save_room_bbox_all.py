5c25f3863fead62a37684392ee63c1fa6dd975f6
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-05-22 00:19:11 -0400

    count files

--- save_room_bbox_all.py
+++ save_room_bbox_all.py
@@ -1,10 +1,10 @@
 import subprocess
 
-for scene_idx in range(3000, 4000):
-    cmd = f"python cli.py run ./scripts/save_room_bboxes.py --scene_idx {scene_idx}"
-    subprocess.run(cmd, shell=True)
-
-
 # for scene_idx in range(3000, 4000):
-#     cmd = f"python cli.py run ./scripts/check_valid_poses.py --scene_idx {scene_idx}"
+#     cmd = f"python cli.py run ./scripts/save_room_bboxes.py --scene_idx {scene_idx}"
 #     subprocess.run(cmd, shell=True)
+
+
+for scene_idx in range(3000, 4000):
+    cmd = f"python cli.py run ./scripts/check_valid_poses.py --scene_idx {scene_idx}"
+    subprocess.run(cmd, shell=True)
