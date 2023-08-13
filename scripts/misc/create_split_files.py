9849aed6dfd589b4232ec300a6d5e17f426c8fa4
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-08-13 12:34:36 -0400

    create scannet 248 split

--- scripts/misc/create_split_files.py
+++ scripts/misc/create_split_files.py
@@ -2,13 +2,24 @@ import os
 import numpy as np
 
 # Load the split file
-split_file = "/wild6d_data/zubair/nerf_rpn/front3d_rpn_data/front3d_split.npz"
+dataset_name = "scannet"
+
+# path = '/wild6d_data/zubair/nerf_rpn/scannet_rpn_data'
+split_file = (
+    "/wild6d_data/zubair/nerf_rpn/${dataset_name}_rpn_data/${dataset_name}_split.npz"
+)
+
+# split_file = "/wild6d_data/zubair/nerf_rpn/front3d_rpn_data/front3d_split.npz"
 
 split = np.load(split_file)
 # Get the list of scenes from the features directory
-features_dir = "/wild6d_data/zubair/MAE_complete_data/features"
 
-out_file = "/wild6d_data/zubair/MAE_complete_data/front3d_split.npz"
+out_dir = "/wild6d_data/zubair/nerf_rpn/scannet_rpn_data_all"
+features_dir = os.path.join(out_dir, "features")
+# features_dir = "/wild6d_data/zubair/nerf_rpn/scannet_rpn_data_all/features"
+
+out_file = os.path.join(out_dir, dataset_name + "_split.npz")
+# out_file = "/wild6d_data/zubair/MAE_complete_data/front3d_split.npz"
 scenes = []
 for file_name in os.listdir(features_dir):
     if file_name.endswith(".npz"):
