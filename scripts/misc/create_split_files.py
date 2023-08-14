9541569e03babb2e7d58f9463a44fa3b886aa0d9
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-08-14 01:41:20 -0400

    arkit scenes train mae3d

--- scripts/misc/create_split_files.py
+++ scripts/misc/create_split_files.py
@@ -4,10 +4,13 @@ import numpy as np
 # Load the split file
 dataset_name = "scannet"
 
+# split_file = (
+#     f"/wild6d_data/zubair/nerf_rpn/{dataset_name}_rpn_data/{dataset_name}_split.npz"
+# )
+
 split_file = (
     f"/wild6d_data/zubair/nerf_rpn/{dataset_name}_rpn_data/{dataset_name}_split.npz"
 )
-
 print("split_file", split_file)
 
 # split_file = "/wild6d_data/zubair/nerf_rpn/front3d_rpn_data/front3d_split.npz"
@@ -15,7 +18,7 @@ print("split_file", split_file)
 split = np.load(split_file)
 # Get the list of scenes from the features directory
 
-out_dir = "/wild6d_data/zubair/nerf_rpn/scannet_rpn_data_all"
+out_dir = "/wild6d_data/zubair/arkitscenes_rpn_data"
 features_dir = os.path.join(out_dir, "features")
 # features_dir = "/wild6d_data/zubair/nerf_rpn/scannet_rpn_data_all/features"
 
@@ -27,8 +30,12 @@ for file_name in os.listdir(features_dir):
         scene_name = os.path.splitext(file_name)[0]
         scenes.append(scene_name)
 
+# Make a random selection of 20 scenes
+selected_scenes = np.random.choice(scenes, size=20, replace=False)
 modified_split = dict(split)
 # Replace train_scenes in the split file with the list of scenes
 modified_split["train_scenes"] = np.array(scenes)
+modified_split["val_scenes"] = selected_scenes[:10]  # First 10 scenes for validation
+modified_split["test_scenes"] = selected_scenes[10:]  # Last 10 scenes for testing
 # Save the modified split file
 np.savez(out_file, **modified_split)
