eeefb42b7f6ab41dd6a10aa8f000e6a60e381105
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-09-14 01:20:34 -0400

    print invalid nerf grid hm3d

--- scripts/misc/check_valid_nerf_grids.py
+++ scripts/misc/check_valid_nerf_grids.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-
+import warnings
 
 min_dim = 50
 feature_dir = "/arkit_data/hm3d_rpn_data"
@@ -14,13 +14,25 @@ npz_files = [
 scenes = [f.split(".")[0] for f in npz_files]
 
 
-def density_to_alpha(density):
+def density_to_alpha(density, scene_name):
+    with warnings.catch_warnings():  # Catch any warnings that occur within this block
+        warnings.filterwarnings(
+            "error", category=RuntimeWarning
+        )  # Raise warnings as errors
+        try:
+            alpha = np.clip(1.0 - np.exp(-np.exp(density) / 100.0), 0.0, 1.0)
+        except RuntimeWarning as e:
+            print(f"Warning occurred in scene {scene_name}: {e}")
+
     return np.clip(1.0 - np.exp(-np.exp(density) / 100.0), 0.0, 1.0)
 
 
 filtered_scenes = []
 filtered_scenes_count = 0
+count = 0
 for scene_name in scenes:
+    count += 1
+    print("count", count)
     # print("scene_name", scene_name)
     # print(
     #     "os.path.join(feature_dir, scene_name + .npz)",
@@ -35,11 +47,11 @@ for scene_name in scenes:
 
     print("density min max", density.min(), density.max())
 
-    alpha = density_to_alpha(density)
+    alpha = density_to_alpha(density, scene_name)
 
-    print("alpha min max", alpha.min(), alpha.max())
+    # print("alpha min max", alpha.min(), alpha.max())
 
-    print("res", res)
+    # print("res", res)
     # print("rgbsigma original", rgbsigma.shape)
 
     # if sum(dim < 50 for dim in res) == 2 or any(dim < 20 for dim in res):
@@ -48,7 +60,7 @@ for scene_name in scenes:
     #     filtered_scenes_count += 1
     #     filtered_scenes.append(scene_name)
 
-    print("==========================================\n\n\n")
+    # print("==========================================\n\n\n")
 print("Invalid number of grids", filtered_scenes_count)
 
 # # Save the filtered scene names to a text file
