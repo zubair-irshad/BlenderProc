76a51352c6d45a56c2f746e21ebfe5a97605c362
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-09-29 18:27:37 -0400

    create split for hm3d

--- scripts/misc/create_split_files.py
+++ scripts/misc/create_split_files.py
@@ -18,7 +18,7 @@ print("split_file", split_file)
 split = np.load(split_file)
 # Get the list of scenes from the features directory
 
-out_dir = "/wild6d_data/zubair/nerf_rpn/hm3d_rpn_data"
+out_dir = "/wild6d_data/zubair/nerf_rpn/hm3d_rpn_data_ft"
 features_dir = os.path.join(out_dir, "features")
 # features_dir = "/wild6d_data/zubair/nerf_rpn/scannet_rpn_data_all/features"
 
@@ -30,12 +30,45 @@ for file_name in os.listdir(features_dir):
         scene_name = os.path.splitext(file_name)[0]
         scenes.append(scene_name)
 
-# Make a random selection of 20 scenes
-selected_scenes = np.random.choice(scenes, size=20, replace=False)
+
+# Create an array of indices representing all scenes
+all_indices = np.arange(len(scenes))
+
+# Shuffle the indices randomly
+np.random.shuffle(all_indices)
+
+# Select the first 20 indices for validation
+val_indices = all_indices[:20]
+
+# Select the next 18 indices for testing
+test_indices = all_indices[20:38]
+
+# The remaining indices are for training
+train_indices = all_indices[38:]
+
+print("len train val test", len(train_indices), len(val_indices), len(test_indices))
+
+# Use the selected indices to create the split
+selected_val_scenes = [scenes[i] for i in val_indices]
+selected_test_scenes = [scenes[i] for i in test_indices]
+selected_train_scenes = [scenes[i] for i in train_indices]
+
+# Now you can update your modified_split dictionary
 modified_split = dict(split)
-# Replace train_scenes in the split file with the list of scenes
-modified_split["train_scenes"] = np.array(scenes)
-modified_split["val_scenes"] = selected_scenes[:10]  # First 10 scenes for validation
-modified_split["test_scenes"] = selected_scenes[10:]  # Last 10 scenes for testing
+modified_split["train_scenes"] = np.array(selected_train_scenes)
+modified_split["val_scenes"] = selected_val_scenes
+modified_split["test_scenes"] = selected_test_scenes
+
+
+# OLD CODE FOR MAE PRETRAINING, UNCOMMENT FOR MAE PRETRAINING
+# # Make a random selection of 20 scenes
+# selected_scenes = np.random.choice(scenes, size=20, replace=False)
+# modified_split = dict(split)
+# # Replace train_scenes in the split file with the list of scenes
+# modified_split["train_scenes"] = np.array(scenes)
+# modified_split["val_scenes"] = selected_scenes[:10]  # First 10 scenes for validation
+# modified_split["test_scenes"] = selected_scenes[10:]  # Last 10 scenes for testing
+
+
 # Save the modified split file
 np.savez(out_file, **modified_split)
